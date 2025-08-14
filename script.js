73f48c57117a70a53b215aaddf66784955affc89
harmeet <159572239+codes-h7@users.noreply.github.com>
2025-08-14 17:56:28 +0530

    add todo list ui and add task feature

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 let Cards = document.querySelectorAll(".elem");
-let Cards_FullPages = document.querySelectorAll(".fullPage");
-let btn = document.querySelectorAll("button");
-// console.log(Cards);
+let Task_input = document.getElementById("Task_input");
+let TaskDetail_input = document.getElementById("TaskDetail_input");
+let addBtn = document.getElementById("AddTaskBtn");
+
+let Taskhold = document.querySelector(".Taskhold");
+addBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  Taskhold.innerHTML += ` <div class="task">
+  <h4>${Task_input.value}</h4>
+  <button class="Delete-btn">Delete</button>
+  </div>`;
+  let Delete_btn = document.querySelectorAll(".Delete-btn");
+  Delete_btn.forEach((elem,index) => {
+    elem.addEventListener("click",()=>{
+      Taskhold.splice(index, 1); // remove from array
+Taskhold[index].remove(); // remove from UI
+
+    })
+  });
+
+  Task_input.value = "";
+  TaskDetail_input.value = "";
+});
+
 Cards.forEach((elem) => {
   elem.addEventListener("click", () => {
     // console.log(elem.id);
     Cards_FullPages[elem.id].style.display = "block";
   });
 });
-btn.forEach((btn) => {
-  console.log(btn);
-  btn.addEventListener("click", () => {
-// console.log(btn.id);
-(Cards_FullPages[btn.id]).style.display="none"
 
+let close_btn = document.querySelectorAll("button");
+let Cards_FullPages = document.querySelectorAll(".fullPage");
+close_btn.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    Cards_FullPages[index].style.display = "none";
   });
 });
+
+// let AddTaskBtn = document.querySelector("#AddTaskBtn");
+// AddTaskBtn.addEventListener("submit", (elem) => {
+// elem.preventDefault()
+// });
